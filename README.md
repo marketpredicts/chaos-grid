87000fcd70d26beb0e9478a78305319796f26f94
Benjamin Manns <benmanns@gmail.com>
2024-03-27 16:01:39 -0400

    Add chaos-grid simulator

---
+++ README.md
@@ -0,0 +1,137 @@
+# chaos-grid
+
+This is a (somewhat) simple brute force analysis of the chaos-grid game from [Sapphire828](https://manifold.markets/EstMtz?r=YmVubWFubnM)'s game [Chaos Grid](https://manifold.markets/EstMtz/-the-chaos-grid-place-your-bets-see?r=YmVubWFubnM) on [Manifold](https://manifold.markets?referrer=benmanns). [Note: I get a small bonus if you sign up with my links]
+
+## The Game
+
+Welcome to The Chaos Grid! At market close, 36 dice will be rolled by the fairlyrandom bot and arranged in a 6x6 grid filling up rows left to right and top to bottom. 1 point will be assigned for each triplet in the grid either vertically or horizontally (diagonals Do Not count). In the cases of quadruplets, pentuplets, or sextuplets, multiple points will be assigned (see example scoring below). Side bets for quadruplets, pentuplets, or sextuplets are also available.
+
+Scoring: Horizontal and vertical
+- Triplet = 1
+- Quadruplet ("Quad") = 2
+- Pentuplet ("Pent") = 3
+- Sextuplet ("Hex") = 4
+
+Range of Scores [0–48]
+
+### Example 1
+
+|       |       |       |       |   |       |
+|-------|-------|-------|-------|---|-------|
+| **1** | **1** | **1** | 3     | 4 | 4     |
+| 4     | 1     | 6     | 4     | 5 | 5     |
+| 2     | 3     | 5     | 5     | 1 | 6     |
+| **3** | **3** | **3** | **3** | 2 | **2** |
+| 6     | 4     | 2     | 2     | 3 | **2** |
+| 5     | 1     | 4     | 3     | 3 | **2** |
+
+Example 1 would resolve: 4=YES, Any Quad=YES
+
+### Example 2
+
+|   |   |   |   |   |   |
+|---|---|---|---|---|---|
+| 1 | 3 | 1 | 3 | 4 | 4 |
+| 4 | 1 | 6 | 4 | 5 | 5 |
+| 2 | 3 | 5 | 5 | 1 | 6 |
+| 3 | 3 | 1 | 3 | 2 | 2 |
+| 6 | 4 | 2 | 2 | 3 | 6 |
+| 5 | 1 | 4 | 3 | 3 | 2 |
+
+Example 2 would resolve: 0=YES
+
+### Example 3
+
+|       |   |   |   |   |   |
+|-------|---|---|---|---|---|
+| 1     | 4 | 2 | 3 | 6 | 5 |
+| **4** | 1 | 3 | 3 | 4 | 1 |
+| **4** | 6 | 5 | 1 | 2 | 4 |
+| **4** | 4 | 5 | 3 | 2 | 3 |
+| **4** | 5 | 1 | 2 | 3 | 3 |
+| **4** | 5 | 6 | 2 | 6 | 2 |
+
+Example 3 would resolve: 3=YES, Any Pent=YES
+
+### Example 4
+
+|   |   |   |   |       |   |
+|---|---|---|---|-------|---|
+| 1 | 4 | 4 | 5 | **5** | 4 |
+| 4 | 1 | 6 | 4 | **5** | 2 |
+| 2 | 3 | 5 | 5 | **5** | 6 |
+| 3 | 3 | 1 | 3 | **5** | 2 |
+| 6 | 4 | 2 | 2 | **5** | 6 |
+| 5 | 1 | 4 | 3 | **5** | 2 |
+
+Example 4 would resolve: 4=YES, Any Hex=YES
+
+What will be the total score of the inaugural Chaos Grid?
+
+I will also award a 500 mana bounty for the first full probability distribution calculation. I thought about this problem for a while and couldn't get very far. What is the probability of each total score 0-48? Private message me your calculations or post them in the comments to help others.
+
+Enjoy! 🎲🎲🎲🎲🎲🎲🎲🎲🎲🎲
+
+## The Analysis
+
+```shell
+$ cargo build --release
+$ time target/release/chaos-grid --iterations 10000000000                                                                                 ~/src/github.com/marketpredicts/chaos-grid
+Score: 0 Count: 2957495307 (29.57%)
+Score: 1 Count: 3278189757 (32.78%)
+Score: 2 Count: 2125954918 (21.26%)
+Score: 3 Count: 1029552537 (10.30%)
+Score: 4 Count: 409613613 (4.10%)
+Score: 5 Count: 140470889 (1.40%)
+Score: 6 Count: 42812032 (0.43%)
+Score: 7 Count: 11871981 (0.12%)
+Score: 8 Count: 3059016 (0.03%)
+Score: 9 Count: 746970 (0.01%)
+Score: 10 Count: 177839 (0.00%)
+Score: 11 Count: 41794 (0.00%)
+Score: 12 Count: 10066 (0.00%)
+Score: 13 Count: 2408 (0.00%)
+Score: 14 Count: 639 (0.00%)
+Score: 15 Count: 162 (0.00%)
+Score: 16 Count: 50 (0.00%)
+Score: 17 Count: 17 (0.00%)
+Score: 18 Count: 3 (0.00%)
+Score: 19 Count: 2 (0.00%)
+Run: 3 Count: 5657646474 (56.57646474%)
+Run: 4 Count: 1216418970 (12.1641897%)
+Run: 5 Count: 153017425 (1.53017425%)
+Run: 6 Count: 15421824 (0.15421824%)
+target/release/chaos-grid --iterations 10000000000  12790.59s user 74.55s system 1110% cpu 19:18.43 total
+```
+
+## Problems
+
+My interpretation was that scoring was independent along columns and rows, however example 4 has two possible interpretations:
+
+The original question:
+
+|   |   |   |   |       |   |
+|---|---|---|---|-------|---|
+| 1 | 4 | 4 | 5 | **5** | 4 |
+| 4 | 1 | 6 | 4 | **5** | 2 |
+| 2 | 3 | 5 | 5 | **5** | 6 |
+| 3 | 3 | 1 | 3 | **5** | 2 |
+| 6 | 4 | 2 | 2 | **5** | 6 |
+| 5 | 1 | 4 | 3 | **5** | 2 |
+
+Example 4 would resolve: 4=YES, Any Hex=YES
+
+My code:
+
+|   |   |       |       |       |   |
+|---|---|-------|-------|-------|---|
+| 1 | 4 | 4     | 5     | **5** | 4 |
+| 4 | 1 | 6     | 4     | **5** | 2 |
+| 2 | 3 | **5** | **5** | **5** | 6 |
+| 3 | 3 | 1     | 3     | **5** | 2 |
+| 6 | 4 | 2     | 2     | **5** | 6 |
+| 5 | 1 | 4     | 3     | **5** | 2 |
+
+Example 4 would resolve: 5=YES, Any Hex=YES
+
+I've [asked for clarification](https://manifold.markets/EstMtz/-the-chaos-grid-place-your-bets-see#qkuhkdysrz) from the game author.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,186 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::BTreeMap;
+
+use ndarray::prelude::*;
+use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
+
+use rayon::prelude::*;
+
+use indicatif::ParallelProgressIterator;
+use ndarray::iter::LanesIter;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of iterations to run
+    #[arg(short, long, default_value_t = 1_000_000)]
+    iterations: u64,
+}
+
+fn runs_at_least(board: &Array<u8, Ix2>, at_least: u64) -> BTreeMap<u64, u64> {
+    let mut runs = BTreeMap::new();
+
+    fn process_axis(lanes: &mut LanesIter<u8, Ix1>, runs: &mut BTreeMap<u64, u64>, at_least: u64) {
+        for lane in lanes {
+            let mut iter = lane.iter();
+            let mut run: u64 = 1;
+            let mut last = iter.next().unwrap();
+
+            for x in iter {
+                if x == last {
+                    run += 1;
+                } else {
+                    if run >= at_least {
+                        runs.entry(run).and_modify(|e| *e += 1).or_insert(1);
+                    }
+                    run = 1;
+                }
+                last = x;
+            }
+            if run >= at_least {
+                runs.entry(run).and_modify(|e| *e += 1).or_insert(1);
+            }
+        }
+    }
+
+    process_axis(&mut (board.rows().into_iter()), &mut runs, at_least);
+    process_axis(&mut (board.columns().into_iter()), &mut runs, at_least);
+
+    runs
+}
+
+fn score_runs(runs: &BTreeMap<u64, u64>) -> u64 {
+    runs.iter().map(|(run, count)| (run - 2) * count).sum()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let iterations = args.iterations;
+
+    let (scores, largest_runs) = (0..iterations)
+        .into_par_iter()
+        .progress_count(iterations)
+        .map(|_| {
+            let board = Array::random((6, 6), Uniform::new(1u8, 7u8));
+            let runs = runs_at_least(&board, 3);
+            let score = score_runs(&runs);
+            let largest_run = runs.keys().max().cloned();
+            (score, largest_run)
+        })
+        .fold(
+            || (BTreeMap::<u64, u64>::new(), BTreeMap::<u64, u64>::new()),
+            |(mut scores, mut largest_runs), (score, largest_run)| {
+                scores.entry(score).and_modify(|e| *e += 1).or_insert(1);
+                if let Some(largest_run) = largest_run {
+                    largest_runs
+                        .entry(largest_run)
+                        .and_modify(|e| *e += 1)
+                        .or_insert(1);
+                }
+                (scores, largest_runs)
+            },
+        )
+        .reduce(
+            || (BTreeMap::new(), BTreeMap::new()),
+            |mut acc, cur| {
+                cur.0.iter().for_each(|(k, v)| {
+                    acc.0.entry(*k).and_modify(|e| *e += v).or_insert(*v);
+                });
+                cur.1.iter().for_each(|(k, v)| {
+                    acc.1.entry(*k).and_modify(|e| *e += v).or_insert(*v);
+                });
+                acc
+            },
+        );
+
+    for (score, count) in scores.iter() {
+        println!(
+            "Score: {score} Count: {count} ({:.2}%)",
+            (count.to_owned() as f64) * 100f64 / (iterations as f64)
+        );
+    }
+    for (run, count) in largest_runs.iter() {
+        println!(
+            "Run: {run} Count: {count} ({}%)",
+            (count.to_owned() as f64) * 100f64 / (iterations as f64)
+        );
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::score_runs;
+
+    #[test]
+    fn example_1() {
+        let board = ndarray::arr2(&[
+            [1, 1, 1, 3, 4, 4],
+            [4, 1, 6, 4, 5, 5],
+            [2, 3, 5, 5, 1, 6],
+            [3, 3, 3, 3, 2, 2],
+            [6, 4, 2, 2, 3, 2],
+            [5, 1, 4, 3, 3, 2],
+        ]);
+        let runs = super::runs_at_least(&board, 3);
+        let score = score_runs(&runs);
+        let largest_run = runs.keys().max().cloned();
+        assert_eq!(score, 4);
+        assert_eq!(largest_run, Some(4));
+    }
+
+    #[test]
+    fn example_2() {
+        let board = ndarray::arr2(&[
+            [1, 3, 1, 3, 4, 4],
+            [4, 1, 6, 4, 5, 5],
+            [2, 3, 5, 5, 1, 6],
+            [3, 3, 1, 3, 2, 2],
+            [6, 4, 2, 2, 3, 6],
+            [5, 1, 4, 3, 3, 2],
+        ]);
+        let runs = super::runs_at_least(&board, 3);
+        let score = score_runs(&runs);
+        let largest_run = runs.keys().max().cloned();
+        assert_eq!(score, 0);
+        assert_eq!(largest_run, None);
+    }
+
+    #[test]
+    fn example_3() {
+        let board = ndarray::arr2(&[
+            [1, 4, 2, 3, 6, 5],
+            [4, 1, 3, 3, 4, 1],
+            [4, 6, 5, 1, 2, 4],
+            [4, 4, 5, 3, 2, 3],
+            [4, 5, 1, 2, 3, 3],
+            [4, 5, 6, 2, 6, 2],
+        ]);
+        let runs = super::runs_at_least(&board, 3);
+        let score = score_runs(&runs);
+        let largest_run = runs.keys().max().cloned();
+        assert_eq!(score, 3);
+        assert_eq!(largest_run, Some(5));
+    }
+
+    #[test]
+    fn example_4() {
+        let board = ndarray::arr2(&[
+            [1, 4, 4, 5, 5, 4],
+            [4, 1, 6, 4, 5, 2],
+            [2, 3, 5, 5, 5, 6],
+            [3, 3, 1, 3, 5, 2],
+            [6, 4, 2, 2, 5, 6],
+            [5, 1, 4, 3, 5, 2],
+        ]);
+        let runs = super::runs_at_least(&board, 3);
+        let score = score_runs(&runs);
+        let largest_run = runs.keys().max().cloned();
+        assert_eq!(score, 4);
+        assert_eq!(largest_run, Some(6));
+    }
 }
